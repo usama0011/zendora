b940caaf178a0d59e0b1394eafbe2386b6695eba
usama0011 <za5232208#gmail.com>
2023-05-31 01:16:15 +0500

    add model

--- pages/gettstarted/index.jsx
+++ pages/gettstarted/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../../styles/Initial.module.css';
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
@@ -19,164 +19,234 @@ const Index = () => {
     const [leaseTiming, setLeaseTiming] = useState('');
     const [hearAboutUs, setHearAboutUs] = useState('');
     const [formError, setFormError] = useState(false);
+    const [isSubmitted, setIsSubmitted] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (name && address && email && telephone && services && industryExperience && leaseTiming && hearAboutUs) {
+        if (
+            name &&
+            address &&
+            email &&
+            telephone &&
+            services &&
+            industryExperience &&
+            leaseTiming &&
+            hearAboutUs
+        ) {
             // Handle form submission logic here
+            setIsSubmitted(true);
             setFormError(false);
-            console.log('Form submitted!');
+            setName('');
+            setAddress('');
+            setEmail('');
+            setTelephone('');
+            setReceiveTextMessages(false);
+            setServices('');
+            setIndustryExperience('');
+            setLicenseInfo({ licenseDate: '', isLicenseValid: false, licenseNumber: '' });
+            setLeaseTiming('');
+            setHearAboutUs('');
         } else {
             setFormError(true);
         }
     };
 
+    useEffect(() => {
+        if (isSubmitted) {
+            setTimeout(() => {
+                setIsSubmitted(false);
+                setLicenseInfo({ licenseNumber: '', isLicenseValid: false });
+            }, 3000);
+        }
+    }, [isSubmitted]);
+
     return (
         <>
             <NavBar />
-            <div className={styles.container}>
-                <h1 className={styles.mytitle}>Tenant Initial Application</h1>
-                <form className={styles.myForm} onSubmit={handleSubmit}>
-                    <div className={styles.form_row}>
-                        <div className={styles.form_field}>
-                            <label className={styles.formLabel}>Name:</label>
-                            <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} required />
+            <div className={styles.gradientBackground}>
+                <div className={styles.container}>
+                    <h1 className={styles.mytitle}>Tenant Initial Application</h1>
+                    {isSubmitted && (
+                        <div className={styles.success_message}>
+                            <p>Form submitted successfully!</p>
                         </div>
-                        <div className={styles.form_field}>
-                            <label className={styles.formLabel}>Address:</label>
-                            <input type="text" name="address" value={address} onChange={(e) => setAddress(e.target.value)} required />
-                        </div>
-                    </div>
-                    <div className={styles.form_row}>
-                        <div className={styles.form_field}>
-                            <label className={styles.formLabel}>Email Address:</label>
-                            <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                        </div>
-                        <div className={styles.form_field}>
-                            <label className={styles.formLabel}>Preferred Telephone Number:</label>
-                            <input
-                                type="number"
-                                name="telephone"
-                                value={telephone}
-                                onChange={(e) => setTelephone(e.target.value)}
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div className={styles.form_row}>
-                        <div className={styles.form_field}>
-                            <label className={styles.formLabel}>How long have you been in your industry?</label>
-                            <input
-                                type="number"
-                                name="industryExperience"
-                                value={industryExperience}
-                                onChange={(e) => setIndustryExperience(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className={styles.form_field}>
-                            <label className={styles.formLabel}>When did you get your license?</label>
-                            <input
-                                type="date"
-                                name="licenseDate"
-                                value={licenseInfo.licenseDate}
-                                onChange={(e) => setLicenseInfo({ ...licenseInfo, licenseDate: e.target.value })}
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div className={`${styles.form_fields} ${styles.mobileView}`}>
-                        <label className={styles.formLabel}>Can you receive text messages?</label>
-                        <div className={styles.checkbox_container}>
-                            <label className={styles.checkbox_label}>
+                    )}
+                    <form className={styles.myForm} onSubmit={handleSubmit}>
+                        <div className={styles.form_row}>
+                            <div className={styles.form_field}>
+                                <label className={styles.formLabel}>Name:</label>
                                 <input
-                                    type="checkbox"
-                                    name="receiveTextMessages"
-                                    checked={receiveTextMessages}
-                                    onChange={() => setReceiveTextMessages(true)}
+                                    type="text"
+                                    name="name"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    required
                                 />
-                                Yes
-                            </label>
-                            <label className={styles.checkbox_label}>
+                            </div>
+                            <div className={styles.form_field}>
+                                <label className={styles.formLabel}>Address:</label>
                                 <input
-                                    type="checkbox"
-                                    name="receiveTextMessages"
-                                    checked={!receiveTextMessages}
-                                    onChange={() => setReceiveTextMessages(false)}
+                                    type="text"
+                                    name="address"
+                                    value={address}
+                                    onChange={(e) => setAddress(e.target.value)}
+                                    required
                                 />
-                                No
-                            </label>
+                            </div>
                         </div>
-                    </div>
-                   
-                   
-                    <div className={styles.form_fiel}>
-                        <label className={styles.formLabel}>Types of Services you provide:</label>
-                        <textarea className={styles.mytextArea} name="services" value={services} onChange={(e) => setServices(e.target.value)} required></textarea>
-                    </div>
-                    <div className={styles.form_row}>
-                        {/* ... */}
-                        <div className={`${styles.form_field} ${styles.license_field}`}> {/* Added styles. */}
-                            <label className={styles.formLabel}>License Number:</label>
-                            <input
-                                type="text"
-                                name="licenseNumber"
-                                value={licenseInfo.licenseNumber}
-                                onChange={(e) => setLicenseInfo({ ...licenseInfo, licenseNumber: e.target.value })}
-                                required
-                            />
+                        <div className={styles.form_row}>
+                            <div className={styles.form_field}>
+                                <label className={styles.formLabel}>Email Address:</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div className={styles.form_field}>
+                                <label className={styles.formLabel}>Preferred Telephone Number:</label>
+                                <input
+                                    type="number"
+                                    name="telephone"
+                                    value={telephone}
+                                    onChange={(e) => setTelephone(e.target.value)}
+                                    required
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <div className={styles.form_row}>
-                        <div  className={`${styles.form_fields} ${styles.mobileView}`}>
-                            <label className={styles.formLabel}>Is your license currently valid?</label>
+                        <div className={styles.form_row}>
+                            <div className={styles.form_field}>
+                                <label className={styles.formLabel}>How long have you been in your industry?</label>
+                                <input
+                                    type="number"
+                                    name="industryExperience"
+                                    value={industryExperience}
+                                    onChange={(e) => setIndustryExperience(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div className={styles.form_field}>
+                                <label className={styles.formLabel}>When did you get your license?</label>
+                                <input
+                                    type="date"
+                                    name="licenseDate"
+                                    value={licenseInfo.licenseDate}
+                                    onChange={(e) => setLicenseInfo({ ...licenseInfo, licenseDate: e.target.value })}
+                                    required
+                                />
+                            </div>
+                        </div>
+                        <div className={`${styles.form_fields} ${styles.mobileView}`}>
+                            <label className={styles.formLabel}>Can you receive text messages?</label>
                             <div className={styles.checkbox_container}>
                                 <label className={styles.checkbox_label}>
                                     <input
                                         type="checkbox"
-                                        name="isLicenseValid"
-                                        checked={licenseInfo.isLicenseValid}
-                                        onChange={() => setLicenseInfo({ ...licenseInfo, isLicenseValid: true })}
+                                        name="receiveTextMessages"
+                                        checked={receiveTextMessages}
+                                        onChange={() => setReceiveTextMessages(true)}
                                     />
                                     Yes
                                 </label>
                                 <label className={styles.checkbox_label}>
                                     <input
                                         type="checkbox"
-                                        name="isLicenseValid"
-                                        checked={!licenseInfo.isLicenseValid}
-                                        onChange={() => setLicenseInfo({ ...licenseInfo, isLicenseValid: false })}
+                                        name="receiveTextMessages"
+                                        checked={!receiveTextMessages}
+                                        onChange={() => setReceiveTextMessages(false)}
                                     />
                                     No
                                 </label>
                             </div>
                         </div>
-                    </div>
-                    <div className={styles.form_field}>
-                        <label className={styles.formLabel}>How soon would you like to begin leasing a suite from us?</label>
-                        <input
-                            type="text"
-                            name="leaseTiming"
-                            value={leaseTiming}
-                            onChange={(e) => setLeaseTiming(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className={styles.form_field}>
-                        <label className={styles.formLabel}>How did you hear about us?</label>
-                        <input
-                            type="text"
-                            name="hearAboutUs"
-                            value={hearAboutUs}
-                            onChange={(e) => setHearAboutUs(e.target.value)}
-                            required
-                        />
-                    </div>
-                    {formError && <p className={styles.error_message}>Please fill in all required fields.</p>}
-                    <button className={styles.mybtn} type="submit" disabled={!name || !address || !email || !telephone || !services || !industryExperience || !leaseTiming || !hearAboutUs}>
-                        Submit
-                    </button>
-                </form>
+                        <div className={styles.form_fiel}>
+                            <label className={styles.formLabel}>Types of Services you provide:</label>
+                            <textarea
+                                className={styles.mytextArea}
+                                name="services"
+                                value={services}
+                                onChange={(e) => setServices(e.target.value)}
+                                required
+                            ></textarea>
+                        </div>
+                        <div className={styles.form_row}>
+                            <div className={`${styles.form_field} ${styles.license_field}`}>
+                                <label className={styles.formLabel}>License Number:</label>
+                                <input
+                                    type="text"
+                                    name="licenseNumber"
+                                    value={licenseInfo.licenseNumber}
+                                    onChange={(e) => setLicenseInfo({ ...licenseInfo, licenseNumber: e.target.value })}
+                                    required
+                                />
+                            </div>
+                        </div>
+                        <div className={styles.form_row}>
+                            <div className={`${styles.form_fields} ${styles.mobileView}`}>
+                                <label className={styles.formLabel}>Is your license currently valid?</label>
+                                <div className={styles.checkbox_container}>
+                                    <label className={styles.checkbox_label}>
+                                        <input
+                                            type="checkbox"
+                                            name="isLicenseValid"
+                                            checked={licenseInfo.isLicenseValid}
+                                            onChange={() => setLicenseInfo({ ...licenseInfo, isLicenseValid: true })}
+                                        />
+                                        Yes
+                                    </label>
+                                    <label className={styles.checkbox_label}>
+                                        <input
+                                            type="checkbox"
+                                            name="isLicenseValid"
+                                            checked={!licenseInfo.isLicenseValid}
+                                            onChange={() => setLicenseInfo({ ...licenseInfo, isLicenseValid: false })}
+                                        />
+                                        No
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.form_field}>
+                            <label className={styles.formLabel}>How soon would you like to begin leasing a suite from us?</label>
+                            <input
+                                type="text"
+                                name="leaseTiming"
+                                value={leaseTiming}
+                                onChange={(e) => setLeaseTiming(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className={styles.form_field}>
+                            <label className={styles.formLabel}>How did you hear about us?</label>
+                            <input
+                                type="text"
+                                name="hearAboutUs"
+                                value={hearAboutUs}
+                                onChange={(e) => setHearAboutUs(e.target.value)}
+                                required
+                            />
+                        </div>
+                        {formError && <p className={styles.error_message}>Please fill in all required fields.</p>}
+                        <button
+                            className={styles.mybtn}
+                            type="submit"
+                            disabled={
+                                !name ||
+                                !address ||
+                                !email ||
+                                !telephone ||
+                                !services ||
+                                !industryExperience ||
+                                !leaseTiming ||
+                                !hearAboutUs
+                            }
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
             </div>
             <Footer />
         </>
